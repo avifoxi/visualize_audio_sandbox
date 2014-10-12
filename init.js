09fd57f98500186e3ba6ddfdb65ca53a35f80510
AviFoxi <frosenox@gmail.com>
2014-10-11 17:14:32 -0700

    request animation frame loop for visualize single tone

--- init.js
+++ init.js
@@ -25,18 +25,23 @@ console.log('hola from init')
 
 	var freqDomain = new Uint8Array(annie.analyser.frequencyBinCount);
 	
-	annie.analyser.getByteFrequencyData(freqDomain);
-	
-	for (var i = 0; i < annie.analyser.frequencyBinCount; i++) {
-	  var value = freqDomain[i];
-	  var percent = value / 256;
-	  var height = HEIGHT * percent;
-	  var offset = HEIGHT - height - 1;
-	  var barWidth = WIDTH/annie.analyser.frequencyBinCount;
-	  var hue = i/annie.analyser.frequencyBinCount * 360;
-	  drawContext.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
-	  drawContext.fillRect(i * barWidth, offset, barWidth, height);
+	function updateCanvas(){
+		annie.analyser.getByteFrequencyData(freqDomain);
+		for (var i = 0; i < annie.analyser.frequencyBinCount; i++) {
+		  var value = freqDomain[i];
+		  var percent = value / 256;
+		  var height = HEIGHT * percent;
+		  var offset = HEIGHT - height - 1;
+		  var barWidth = WIDTH/annie.analyser.frequencyBinCount;
+		  var hue = i/annie.analyser.frequencyBinCount * 360;
+		  drawContext.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+		  drawContext.fillRect(i * barWidth, offset, barWidth, height);
+		}
+		window.requestAnimationFrame(updateCanvas);
 	}
+	
+	window.requestAnimationFrame(updateCanvas);
+
 
 
 
